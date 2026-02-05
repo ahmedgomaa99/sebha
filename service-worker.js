58a3d9c30c0d018824b866e644dba6b06821ba72
Ahmed Gomaa <112859193+ahmedgomaa99@users.noreply.github.com>
2026-02-05 23:21:49 +0200

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "sebha-cache-v1"
+const CACHE_NAME = "sebha-cache-v2"
 
 const FILES_TO_CACHE = [
   "./",
@@ -51,6 +51,7 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
+
 
 
 
